6dc94a856e4d77a8f851a7f4840c9f4f0ac4113a
@SoulOG <285963@supinfo.com>
2021-03-14 23:37:48 +0000

    Ajout de la methode de retournement de piece a la classe Coin

--- classCoin.py
+++ classCoin.py
@@ -1,4 +1,8 @@
 class Coin:
+    """ Implementation de la classe Coin avec un attribut de type booléen
+     True correspond à Face
+      False correspond à Pile
+      """
     def __init__(self, face=True):
         self.__face = face
 
@@ -6,5 +10,8 @@ class Coin:
         self.__face = face
 
     def getFace(self):
-        return self.__face
+        return bool(self.__face)
+
+    def turnCoin(self):
+        self.__face = False
 
