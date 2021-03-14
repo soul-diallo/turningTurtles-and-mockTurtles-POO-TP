8dfe025d50b04941ac240a33b1d47d0acc1ccbb1
@SoulOG <285963@supinfo.com>
2021-03-14 23:17:13 +0000

    Fin de l'implementation de la classe coin

--- classCoin.py
+++ classCoin.py
@@ -1,1 +1,10 @@
+class Coin:
+    def __init__(self, face=True):
+        self.__face = face
+
+    def setFace(self,face):
+        self.__face = face
+
+    def getFace(self):
+        return self.__face
 
