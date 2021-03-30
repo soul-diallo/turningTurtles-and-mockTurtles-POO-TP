85e92b51cfb9cdb29aa1fffd1d1e1b45d69c33b4
@SoulOG <285963@supinfo.com>
2021-03-30 15:32:11 +0000

    Implementation du constructeur et des attributs de la classe

--- classTurningTurtles.py
+++ classTurningTurtles.py
@@ -5,14 +5,16 @@ import classRow
 class turningTurtles:
     # Attributs de la classe
     joueurs = []
-    for i in range(1, 3):
-        print("Nom du Joueur", i)
-        name = input()
-        joueurs.append(classPlayer.Player(name))
+
+    r = classRow.Row()
 
     # Constructeur de la classe
     def __init__(self, nbrPiece):
         self.__nbrPiece = nbrPiece
+        for i in range(1, 3):
+            print("Nom du Joueur", i)
+            name = input()
+            self.joueurs.append(classPlayer.Player(name))
 
     def afficher(self):
         print(self.joueurs[0].getName())
